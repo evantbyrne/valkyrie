0346c46f20300f59fd14eba4bc5afdc17aba7f3e
Evan Byrne <evantbyrne@gmail.com>
2012-09-23 15:54:37 -0400

    Added prepare(sql, params), prepare(sql), and execute(sql) methods to Connection

--- com/beakerstudio/valkyrie/Connection.java
+++ com/beakerstudio/valkyrie/Connection.java
@@ -1,9 +1,12 @@
 package com.beakerstudio.valkyrie;
 
 import java.io.File;
+import java.util.Vector;
 
 import com.almworks.sqlite4java.SQLiteConnection;
 import com.almworks.sqlite4java.SQLiteException;
+import com.almworks.sqlite4java.SQLiteStatement;
+import com.beakerstudio.valkyrie.sql.Base;
 
 /**
  * Connection Class
@@ -87,6 +90,58 @@ public class Connection {
 	public void close_connection() {
 		
 		this.sqlite.dispose();
+		
+	}
+	
+	/**
+	 * Prepare
+	 * @param String SQL
+	 * @param Vector<String> Parameters to bind
+	 * @return SQLiteStatement
+	 * @throws SQLiteException
+	 */
+	public SQLiteStatement prepare(String sql, Vector<String> params) throws SQLiteException {
+		
+		SQLiteStatement st = this.sqlite.prepare(sql);
+		
+		if(params != null) {
+		
+			int i = 1;
+			for(String param : params) {
+				
+				st.bind(i, param);
+				i++;
+				
+			}
+		
+		}
+		
+		return st;
+		
+	}
+	
+	/**
+	 * Prepare
+	 * @param String SQL
+	 * @return SQLiteStatement
+	 * @throws SQLiteException
+	 */
+	public SQLiteStatement prepare(String sql) throws SQLiteException {
+		
+		return this.prepare(sql, null);
+		
+	}
+	
+	/**
+	 * Execute
+	 * @param String SQL
+	 * @return this
+	 * @throws SQLiteException
+	 */
+	public Connection execute(String sql) throws SQLiteException {
+		
+		this.sqlite.exec(sql);
+		return this;
 		
 	}
 	
